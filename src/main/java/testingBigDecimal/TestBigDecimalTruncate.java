9b1c766c2b640ef50c86d6741ec519d5ccf3d6c5
Jhonata Costa <costajhonata8@gmail.com>
2024-05-10 21:59:41 -0300

    Testing BigDecimal truncate

---
+++ src/main/java/testingBigDecimal/TestBigDecimalTruncate.java
@@ -0,0 +1,27 @@
+package testingBigDecimal;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class TestBigDecimalTruncate {
+    public static void main(String[] args) {
+        BigDecimal testValue1 = BigDecimal.valueOf(10.5781653);
+        testValue1 = testValue1.setScale(4, RoundingMode.DOWN);
+
+        BigDecimal testValue2 = testValue1.setScale(4, RoundingMode.UP);
+        BigDecimal testValue3 = testValue1.setScale(4, RoundingMode.CEILING);
+        BigDecimal testValue4 = testValue1.setScale(4, RoundingMode.FLOOR);
+
+        BigDecimal testValue5 = testValue1.setScale(5, RoundingMode.HALF_DOWN);
+        BigDecimal testValue6 = testValue1.setScale(5, RoundingMode.HALF_UP);
+        BigDecimal testValue7 = testValue1.setScale(5, RoundingMode.HALF_EVEN);
+
+        System.out.println("Test value 1: " + testValue1);
+        System.out.println("Test value 2: " + testValue2);
+        System.out.println("Test value 3: " + testValue3);
+        System.out.println("Test value 4: " + testValue4);
+        System.out.println("Test value 5: " + testValue5);
+        System.out.println("Test value 6: " + testValue6);
+        System.out.println("Test value 7: " + testValue7);
+    }
+}
